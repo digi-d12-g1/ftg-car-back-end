1a4c04cad0eb5806b1fb45b83cd995d9d18d6424
gmanchelin <gmanchelin@gmail.com>
2023-02-06 17:26:33 +0100

    Correction addVehicle
    Ajout du @RequestBody dans les param

--- src/main/java/com/ftgcar/controllers/VehicleController.java
+++ src/main/java/com/ftgcar/controllers/VehicleController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ftgcar.dto.VehicleDto;
@@ -23,7 +25,7 @@ public class VehicleController {
     }
 
     @PostMapping("/add")
-    public VehicleDto addVehicle(VehicleDto vehicleDto) {
+    public VehicleDto addVehicle(@RequestBody VehicleDto vehicleDto) {
         return vehicleService.addVehicle(vehicleDto);
     }
 
