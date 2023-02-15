e152846153ba1084757780bb7770aa344914c051
gmanchelin <gmanchelin@gmail.com>
2023-02-15 16:21:28 +0100

    Blocages de résa (un employee ne peut pas résa 2 fois le même covoit, ni résa sur un covoit qu'il a créé)

--- src/main/java/com/ftgcar/dao/BookingAdvertCarpoolingRepository.java
+++ src/main/java/com/ftgcar/dao/BookingAdvertCarpoolingRepository.java
@@ -10,7 +10,13 @@ import com.ftgcar.entity.BookingAdvertCarpooling;
 
 public interface BookingAdvertCarpoolingRepository extends JpaRepository<BookingAdvertCarpooling, Long>{
     
+
+    @Query(value = "SELECT * FROM public.booking_advert_carpooling booking WHERE booking.id_employee = (:employeeID) AND booking.id_advert_carpooling = (:advertCarpoolingID)", nativeQuery = true)
+    List<BookingAdvertCarpooling> findAllByIdEmployeeAndIdAdvertCarpooling(@Param("employeeID") Long idEmployee, @Param("advertCarpoolingID") Long idAdvertCarpooling);
+
     @Query(value = "SELECT * FROM public.booking_advert_carpooling booking WHERE booking.id_employee = (:employeeID)", nativeQuery = true)
     List<BookingAdvertCarpooling> findAllByIdEmployee(@Param("employeeID") Long idEmployee);
+
+    
 
 }
